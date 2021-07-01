2e1ab43781f908bbfc3a90ed8f5e3c3f64099e8e
Tanner <treynolds4@student.yosemite.edu>
2021-07-01 11:54:02 -0700

    Update layout.public.php

--- core/templates/layout.public.php
+++ core/templates/layout.public.php
@@ -8,6 +8,7 @@
 		p($theme->getTitle());
 		?>
 	</title>
+	<link rel="stylesheet" href="https://cdn.tanners.space/titlebar.css">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 	<?php if ($theme->getiTunesAppId() !== '') { ?>
